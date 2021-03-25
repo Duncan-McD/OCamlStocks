63230d5ab37caf16c97ffb15bdaa955f86f3537d
Duncan-McD <dam342@cornell.edu>
2021-03-25 02:04:29 -0400

    Made amount a label parameter so we didn't need two functions

--- scraper.mli
+++ scraper.mli
@@ -10,6 +10,5 @@ val subreddit : t -> string
 val title : post -> string
 val body : post -> string
 
-(* scrapes subreddit w/ cutoff -> t *)
-val scrape_default : string -> t
-val scape_amount : string -> int -> t
+(* scrapes subreddit w/ optional cutoff -> t *)
+val scrape_default : amount:int -> string -> t
