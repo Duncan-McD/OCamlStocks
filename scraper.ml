49669e2130a377806530f0a4eaf2e4fe407f7b19
Duncan-McD <dam342@cornell.edu>
2021-03-25 18:15:29 -0400

    Added documenation and scraper.ml

--- scraper.ml
+++ scraper.ml
@@ -1,4 +1,19 @@
+type post = {
+  post_name: string;
+  body: string;
+}
 
+type subreddit = {
+  subreddit_name: string;
+  posts: post list;
+}
 
+let posts (scraper:subreddit) : post list = failwith "unimplemented" 
 
-let scrape ?(amount=100) (subreddit:string) = failwith "unimplemented"
+let subreddit_name (scraper:subreddit) : string = failwith "unimplemented"
+
+let title (post:post) : string = failwith "unimplemented"
+
+let body (post:post) : string = failwith "unimplemented"
+
+let scrape ?(amount=100) (subreddit:string) : subreddit = failwith "unimplemented"
--- scraper.mli
+++ scraper.mli
@@ -1,14 +1,34 @@
-type t
+(** Representation of a Subreddit Scraper *)
+
+type subreddit
+(** the abstract type of a scraped subreddit *)
 
 type post
+(** The abstract type of a scraped reddit post *)
 
-val posts : t -> post list
+exception SubredditNotFound of string
+(** Raised when a subreddit is not found matching the url *)
 
-val subreddit : t -> string
+exception TooManyPostsRequested of int
+(** Raised when a user requests to parse more posts than there is in the subreddit*)
 
-(* getters *)
+val posts : subreddit -> post list
+(** [posts r] is a list of all scraped posts in subreddit [r] *)
+
+val subreddit_name : subreddit -> string
+(** [subreddit r] is the name of subreddit [r] *)
+
 val title : post -> string
-val body : post -> string
+(** [title p] is the title of a post [p] *)
 
-(* scrapes subreddit w/ optional cutoff -> t *)
-val scrape: ?amount:int -> string -> t
+val body : post -> string
+(** [body p] is the body of a post [p] *)
+
+val scrape : ?amount:int -> string -> subreddit
+(** [scrape s] parses the subreddit indicated in string [s] to the 
+    default 100 posts. [scrape s ~i] parses the subreddit indicated 
+    in the string [s] to the requested [i] posts
+    Requires: [s] is a valid url to a subreddit
+    Raises [SubredditNotFound s] if there is no subreddit matching the url [s]
+    Raises [TooManyPostsRequested i] if there is not enough posts in subreddit 
+    to meet request.*)
