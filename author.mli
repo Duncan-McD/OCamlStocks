256e8d7a0b34d3085b7d0223701a5aa6995084ea
Duncan-McD <dam342@cornell.edu>
2021-03-25 18:30:42 -0400

    updated author.mli

---
+++ author.mli
@@ -0,0 +1,10 @@
+(** CS 3110 Final Project - Ocaml Stock Bot
+
+    @authors 
+    Duncan McDonald (dam342), 
+    Peter Bell (pnb36), 
+    Matthew Chan (mc2438), 
+    Miguel Roberts (mar436) *)
+
+val hours_worked : int
+(** [hours_worked] is the number of hours you worked on this assignment. *)
