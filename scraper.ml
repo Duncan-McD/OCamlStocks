bbc39748c9c1a614df7aafcd1a534d2f2aaa1aca
Duncan McDonald <dam342@cornell.edu>
2021-03-26 00:52:39 -0400

    Scraper interface and skeleton module (#16)
    * Initial Scraper Interface
    
    * Develop scraper interface
    
    * Made amount a label parameter so we didn't need two functions
    
    * added forgotten ?
    
    * updated scraper.mli
    
    * Added documenation and scraper.ml
    
    * added exceptions to scraper.ml
    
    Co-authored-by: Matthew Chan <mc2438@cornell.edu>

--- scraper.ml
+++ scraper.ml
@@ -1,4 +1,18 @@
+type post = { post_name : string; body : string }
 
+type subreddit = { subreddit_name : string; posts : post list }
 
+exception SubredditNotFound of string
 
-let scrape ?(amount=100) (subreddit:string) = failwith "unimplemented"
+exception TooManyPostsRequested of int
+
+let posts (scraper : subreddit) : post list = failwith "unimplemented"
+
+let subreddit_name (scraper : subreddit) : string = failwith "unimplemented"
+
+let title (post : post) : string = failwith "unimplemented"
+
+let body (post : post) : string = failwith "unimplemented"
+
+let scrape ?(amount = 100) (subreddit : string) : subreddit =
+  failwith "unimplemented"
