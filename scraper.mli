92c8120cdf006de009786955ad066e030484ae3f
Matthew Chan <mc2438@cornell.edu>
2021-03-31 17:30:49 -0400

    Conform documentation style

--- scraper.mli
+++ scraper.mli
@@ -1,10 +1,10 @@
 (** Representation of a Subreddit Scraper *)
 
-type subreddit
 (** the abstract type of a scraped subreddit *)
+type subreddit
 
-type post
 (** The abstract type of a scraped reddit post *)
+type post
 
 (** The type representing the allowed creation time of posts to scrape *)
 type time = Now | Today | ThisWeek | ThisMonth | ThisYear | AllTime
@@ -12,31 +12,30 @@ type time = Now | Today | ThisWeek | ThisMonth | ThisYear | AllTime
 (** The type representing the order of posts on reddit*)
 type subreddit_ordering = Hot | New | Rising | Top of time
 
-exception SubredditNotFound of string
 (** Raised when a subreddit is not found matching the url *)
+exception SubredditNotFound of string
 
-exception TooManyPostsRequested of int
 (** Raised when a user requests to parse more posts than there is in the subreddit*)
+exception TooManyPostsRequested of int
 
-val posts : subreddit -> post list
 (** [posts r] is a list of all scraped posts in subreddit [r] *)
+val posts : subreddit -> post list
 
-val subreddit_name : subreddit -> string
 (** [subreddit r] is the name of subreddit [r] *)
+val subreddit_name : subreddit -> string
 
-val title : post -> string
 (** [title p] is the title of a post [p] *)
+val title : post -> string
 
-val body : post -> string
 (** [body p] is the body of a post [p] *)
+val body : post -> string
 
-val score : post -> int
 (** [score p] is the score of a post [p] (upvotes - downvotes) *)
+val score : post -> int
 
-val upvote_ratio : post -> float
 (** [upvote_ratio p] is the ratio of upvotes to total votes a post [p] has *)
+val upvote_ratio : post -> float
 
-val scrape : ?amount:int -> ?ordering:subreddit_ordering -> string -> subreddit
 (** [scrape s] parses the subreddit indicated in string [s] to the 
     default 100 posts and with the default New ordering on reddit
     [scrape s ~i] parses the subreddit indicated in the string [s] 
@@ -46,3 +45,4 @@ val scrape : ?amount:int -> ?ordering:subreddit_ordering -> string -> subreddit
     Raises [SubredditNotFound s] if there is no subreddit matching [s]
     Raises [TooManyPostsRequested i] if there is not enough posts in subreddit 
     to meet request.*)
+val scrape : ?amount:int -> ?ordering:subreddit_ordering -> string -> subreddit
