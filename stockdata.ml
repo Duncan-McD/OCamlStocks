44720ed6c660d1d74be8aee17d6146df9247a55f
Duncan-McD <dam342@cornell.edu>
2021-04-01 13:59:28 -0400

    added rest of stockdata comments

--- stockdata.ml
+++ stockdata.ml
@@ -3,10 +3,12 @@ module M = Agent.Monad
 open M.Infix
 open Soup
 
+(** [get_soup site] is the soup node representation of website [site] *)
 let get_soup (site : string) : soup node M.m =
   Agent.get site >|= Agent.HttpResponse.page >|= Page.soup
 
-let get_one_tag (tag : string) (soup_node : soup node M.m) : string =
+(** [get_full_page_json soup_node] is the json representation of [soup_node] *)
+  let get_one_tag (tag : string) (soup_node : soup node M.m) : string =
   match M.run (Agent.init ()) soup_node with
   | _, node ->
       node |> (*select_one ".YDC-Lead" |> require |> *) select_one tag
@@ -14,19 +16,24 @@ let get_one_tag (tag : string) (soup_node : soup node M.m) : string =
       |> require
       |> leaf_text |> require
 
+(* css selector for stock day change *)
 let change_selector =
   "#quote-header-info > div.My\(6px\).Pos\(r\).smartphone_Mt\(6px\) > \
    div.D\(ib\).Va\(m\).Maw\(65\%\).Ov\(h\) > div > \
    span.Trsdu\(0\.3s\).Fw\(500\).Pstart\(10px\).Fz\(24px\)"
 
+(* css selector for stock value *)
 let value_selector =
   "#quote-header-info > div.My\(6px\).Pos\(r\).smartphone_Mt\(6px\) > \
    div.D\(ib\).Va\(m\).Maw\(65\%\).Ov\(h\) > div > \
    span.Trsdu\(0\.3s\).Fw\(b\).Fz\(36px\).Mb\(-4px\).D\(ib\)"
 
+(** [scrape_value l] is the current value of the stock in the link [l] *)
 let scrape_value link =
   float_of_string (get_one_tag value_selector (get_soup link))
 
+(** [scrape_value l] is the current change of the stock in the link [l] 
+    since open *)
 let scrape_change link =
   let change = get_one_tag change_selector (get_soup link) in
   let change_end = String.index change '(' in
