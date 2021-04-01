e0b2da0123893281e16d71f88b23729c8b1c0e66
Mars7543 <miguelaroberts09@gmail.com>
2021-04-01 17:55:16 -0400

    Parser implementation complete (except for connotation) (#25)
    * Start parser.ml functions
    
    * Refactor parser.mli and implement all functions (parse currently just returns empty hashtable
    
    * Theoritcally add a working parser implementation
    
    * add documentation comment to 'update_one_stock'
    
    * Resolve type mismatches in parser
    
    * Fix data function type
    
    * Fix connotation type mismatch
    
    * reverse thingy
    
    Co-authored-by: Duncan-McD <dam342@cornell.edu>

--- parser.ml
+++ parser.ml
@@ -1,15 +1,65 @@
-type stock = unit
+type post = { score : int; upvote_ratio : float; connotation : float }
 
-type post = unit
+type stocks = (string, post list) Hashtbl.t
 
-let parse subreddit = failwith "Unimplemented"
+let connotation post = 0.0
 
-let name stock = failwith "Unimplemented"
+(*  [update_one_stock stock_name post stocks] is [stocks] with the new post data 
+    added to the stock [stock_name] *)
+let update_one_stock stock_name post stocks =
+  if Hashtbl.mem stocks stock_name then
+    let posts = Hashtbl.find stocks stock_name in
+    Hashtbl.replace stocks stock_name (post :: posts)
+  else Hashtbl.add stocks stock_name [ post ];
+  stocks
 
-let data stock = failwith "Unimplemented"
+(*  [update_stocks text post stocks_seen stocks] is [stocks] 
+    updated by reading each word in [text] and, if the word is a stock,
+    updating that stock with the post data [post] 
+    
+    [stocks_seen] keeps track of each stock read in the post to avoid 
+    adding [post] to the same stock more than once *)
+let rec update_stocks text post stocks_seen stocks =
+  match text with
+  | [] -> stocks
+  | w :: t ->
+      if
+        Cashset.is_stock_name w
+        && Bool.not (List.exists (fun p -> p = w) stocks_seen)
+      then
+        let stocks' = update_one_stock w post stocks in
+        update_stocks t post (w :: stocks_seen) stocks'
+      else update_stocks t post stocks_seen stocks
 
-let score post = failwith "Unimplemented"
+(* TODO: stocks $GME and GME will be put treated as separate stocks with their own post data fix *)
+(* [populate_stocks] is the [stocks] hashtable created from the data in [posts] *)
+let rec populate_stocks posts stocks =
+  match posts with
+  | [] -> stocks
+  | p :: t ->
+      let post =
+        {
+          score = Scraper.score p;
+          upvote_ratio = Scraper.upvote_ratio p;
+          connotation = connotation p;
+        }
+      in
+      let title = Scraper.title p in
+      let body = Scraper.body p in
+      let text = title ^ " " ^ body |> String.split_on_char ' ' in
+      let stocks' = update_stocks text post [] stocks in
+      populate_stocks t stocks'
 
-let ratio post = failwith "Unimplemented"
+let parse subreddit =
+  let stocks = Hashtbl.create 500 in
+  let posts = Scraper.posts subreddit in
+  populate_stocks posts stocks
 
-let connotation post = failwith "Unimplemented"
+let stock_names stocks =
+  Hashtbl.fold (fun name posts stock_names -> name :: stock_names) stocks []
+
+let data = Hashtbl.find
+
+let upvote_score post = post.score
+
+let upvote_ratio post = post.upvote_ratio
--- parser.mli
+++ parser.mli
@@ -1,29 +1,28 @@
 (** Representation of a Subreddit Parser *)
 
-(** The abstract type of a stock *)
-type stock
-
 (** The abstract type of relevant data about a post *)
 type post
+
+(** The abstract type of the list of stocks and their relevant data *)
+type stocks
 
 (** [parse sub] is a list of stocks. It parses the scraped subreddit [sub] to 
     find mentioned stocks and  gets relevant data from each post it is 
     mentioned in *)
-val parse : Scraper.subreddit -> stock list
+val parse : Scraper.subreddit -> stocks
 
-(** [name stock] is the stock ticker symbol of the given [stock] *)
-val name : stock -> string
+(** [stock_names stocks] is the list of stock ticker symbols of all stocks in [stocks] *)
+val stock_names : stocks -> string list
 
-(** [data stock] is the relevant data associated with [stock] for a post it 
-    is mentioned in *)
-val data : stock -> post list
+(** [data stock_name] is the relevant data associated with [stock] *)
+val data : stocks -> string -> post list
 
 (** [score post] is the upvotes minus downvotes of [post] *)
-val score : post -> int
+val upvote_score : post -> int
 
-(** [ratio post] is the ratio of upvotes to total votes of [post] *)
-val ratio : post -> float
+(** [upvote_ratio post] is the ratio of upvotes to total votes of [post] *)
+val upvote_ratio : post -> float
 
 (** [connotation post] is the connotation of language of [post] 
-    The range of possible connotations is -1 to 1 from worst to best*)
+    The range of possible connotations is -1 to 1 from worst to best *)
 val connotation : post -> float
