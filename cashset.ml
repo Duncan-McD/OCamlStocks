4c41e26955568ae7599d5026830ce045dcbabc63
PeterNBell <32136284+PeterNBell@users.noreply.github.com>
2021-04-01 19:58:19 -0400

    Cashset documentation (#30)
    * added comments
    
    * fixed error
    
    Co-authored-by: pnb36 <pnb36@cornell.edu>

--- cashset.ml
+++ cashset.ml
@@ -1,60 +1,50 @@
-(* convert csv file to a a hashmap inside of a .ml file *)
-(* get each line of the Csvfile until there is no longer a line*)
-(* call add_to_hastbl on each line*)
-let cashtable = Hashtbl.create 7337
+(** [cashtable] is the hashtable that stores the hashed names of stocks as keys with value 0 *)
+let cashtable = Hashtbl.create 10624
 
+(** [word_hashtable] is the hashtable that stores the hashed names of common words that may be the same as a stock ticker 0 *)
 let word_hashtbl = Hashtbl.create 50
 
 let stock_file = "stocks.csv"
 
 let words_file = "commonwords.csv"
 
+(** [parse_line str] is the string that occurs before the ',' in a given string *)
 let rec parse_line str =
   let endat = String.index str ',' in
   String.sub str 0 endat
 
+(** [build_word_hashtable csv_line] adds [csv_line] to [word_hashtable]*)
 let build_word_hashtable (csv_line : string) =
   Hashtbl.add word_hashtbl (Hashtbl.hash csv_line) 0
 
+(** [add_to_stock_hashtbl csv_line] adds [csv_line] to [cashtable] and adds ($ ^ [csv_line]) to [cashtable] if [csv_line] is not in [word_hashtl] *)
 let add_to_stock_hashtbl (csv_line : string) =
   if Hashtbl.mem word_hashtbl (Hashtbl.hash csv_line) = false then
     Hashtbl.add cashtable (Hashtbl.hash csv_line) 0;
   Hashtbl.add cashtable (Hashtbl.hash ("$" ^ csv_line)) 0
 
+(** constructs [word_hashtbl] from commonwords.csv; constructs [cashtable]*)
 let () =
-  (* Read file and display the first line *)
   let ic = open_in words_file in
   for i = 1 to 49 do
     try
-      (* read line, discard \n *)
       let line = input_line ic in
-      (*add parsed line to hashtbl*)
       line |> parse_line |> build_word_hashtable
     with e ->
-      (* some unexpected exception occurs *)
       close_in_noerr ic;
-      (* emergency closing *)
       raise e
   done;
   close_in ic;
   let ic = open_in stock_file in
-  for i = 1 to 7337 do
+  for i = 1 to 10624 do
     try
-      (* read line, discard \n *)
       let line = input_line ic in
-      (*add parsed line to hashtbl*)
       line |> parse_line |> add_to_stock_hashtbl
     with e ->
-      (* some unexpected exception occurs *)
       close_in_noerr ic;
-      (* emergency closing *)
       raise e
   done;
   close_in ic
 
+(** [is_stock_name s] is true if [s] is the name of a stock on the stock market *)
 let is_stock_name (name : string) = Hashtbl.mem cashtable (Hashtbl.hash name)
-
-(* exit with error: files are closed but
-   channels are not flushed *)
-
-(* normal exit: all channels are flushed and closed *)
