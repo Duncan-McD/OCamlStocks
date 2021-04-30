5c4d21eab70a3c491ccc4d47becd40bee2d66bd4
Matthew Chan <mc2438@cornell.edu>
2021-04-29 22:37:02 -0400

    Implement connotation demoability

--- parser.ml
+++ parser.ml
@@ -2,19 +2,22 @@ type post = { score : int; upvote_ratio : float; connotation : float }
 
 type stocks = (string, float * post list) Hashtbl.t
 
-let vader_sent post = 
+let connotation_str str = 
   Py.initialize ();
-  let postText = Scraper.title post ^ "\n" ^ Scraper.body post in
   let vader = Py.import "vaderSentiment.vaderSentiment" in
   let sentAnalyzer = Py.Module.get_function vader "SentimentIntensityAnalyzer" [||] in
   let polarityScore = Py.Module.get_function sentAnalyzer "polarity_scores" in
-  let resultDict = polarityScore [| Py.String.of_string postText |] in
+  let resultDict = polarityScore [| Py.String.of_string str |] in
   let compound = Py.Dict.get_item resultDict (Py.String.of_string "compound") in
   let result = match compound with
   | None -> failwith "impossible"
   | Some x -> Py.Float.to_float x in
   Py.finalize ();
   result
+
+let connotation_post post = 
+  let postText = Scraper.title post ^ "\n" ^ Scraper.body post in
+  connotation_str postText
 
 let history_score stock_data =
   let rating =
