73f6d400c6af4d54de4b1245a8ca4c87825b1796
Duncan-McD <dam342@cornell.edu>
2021-05-04 02:34:04 -0400

    skeleton

---
+++ portfolio.ml
@@ -0,0 +1,29 @@
+type stock = unit
+
+type t = unit
+
+let portfolio_gain_loss (portfolio : t) : float = failwith "unimplemented"
+
+let portfolio_gain_loss_day (portfolio : t) : float = failwith "unimplemented"
+
+let net_worth (portfolio : t) : float = failwith "unimplemented"
+
+let liquidity (portfolio : t) : float = failwith "unimplemented"
+
+let shares (stock : stock) : int = failwith "unimplemented"
+
+let stock_gain_loss (stock : stock) : float = failwith "unimplemented"
+
+let stock_gain_loss_day (stock : stock) : float = failwith "unimplemented"
+
+let list_of_tickers (portfolio : t) : string list = failwith "unimplemented"
+
+let list_of_stocks (portfolio : t) : stock list = failwith "unimplemented"
+
+let change_ticker_shares (portfolio : t) (ticker : string) (shares : int)
+    (liquidity : float) : t =
+  failwith "unimplemented"
+
+let change_stock_shares (portfolio : t) (stock : stock) (shares : int)
+    (liquidity : float) : t =
+  failwith "unimplemented"
