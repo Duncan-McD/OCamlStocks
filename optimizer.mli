2e0404f93bafacf2544a9379baf04658ee59e0d0
Matthew Chan <47549983+MattChann@users.noreply.github.com>
2021-05-13 18:32:08 -0400

    Feature/algorithm decisions (#58)
    * Documentation and edge handling
    
    * AImplement normalization and decision making outputs

---
+++ optimizer.mli
@@ -0,0 +1,3 @@
+(** [constants ()] is a tuple of constants to be used in the algorithm. These
+    constants are optimized as best as possible. *)
+val constants : unit -> float * float * float * float
