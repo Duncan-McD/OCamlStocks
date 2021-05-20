188ae9e4fa0ec1a7ac639519c445c20c01c74be5
Mars7543 <miguelaroberts09@gmail.com>
2021-05-20 19:42:16 -0400

    Add Parser.join_stocks and update algoritm/algoritmdemo to take in multiple subreddits (#61)

--- algorithm.mli
+++ algorithm.mli
@@ -1,17 +1,21 @@
-(** [get_stocks subreddit] is a pair of lists where the first
+val get_stocks : Scraper.subreddit list -> (string * float) list * string list
+(** [get_stocks subreddit_list] is a pair of lists where the first
     element is a weighted list of stocks where the weights are at most 1 and
     correspond to the recommended percentage of money to be invested in each
     particular stock. The second entry is a list of stocks recommended to be
     sold. The stocks found in the pair are all stocks mentioned within
-    [subreddit] and processed using optimized constants. *)
-val get_stocks :
-  Scraper.subreddit -> (string * float) list * string list
+    [subreddit_list] and processed using optimized constants. *)
 
-(** [get_stocks_consts x y q w subreddit] is a pair of lists where the first
+val get_stocks_consts :
+  float ->
+  float ->
+  float ->
+  float ->
+  Scraper.subreddit list ->
+  (string * float) list * string list
+(** [get_stocks_consts x y q w subreddit_list] is a pair of lists where the first
     element is a weighted list of stocks where the weights are at most 1 and
     correspond to the recommended percentage of money to be invested in each
     particular stock. The second entry is a list of stocks recommended to be
     sold. The stocks found in the pair are all stocks mentioned within
-    [subreddit] and processed using constants [x], [y], [q], and [w]. *)
-val get_stocks_consts : float -> float -> float -> float -> Scraper.subreddit ->
-  (string * float) list * string list
+    [subreddit_list] and processed using constants [x], [y], [q], and [w]. *)
