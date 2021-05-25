4a35de27c439ba95850ed7f2a66422275eb48147
Duncan-McD <dam342@cornell.edu>
2021-05-24 23:10:53 -0400

    remove dead code

--- optimizer.mli
+++
@@ -1,3 +0,0 @@
-(** [constants ()] is a tuple of constants to be used in the algorithm. These
-    constants are optimized as best as possible. *)
-val constants : unit -> float * float * float * float
